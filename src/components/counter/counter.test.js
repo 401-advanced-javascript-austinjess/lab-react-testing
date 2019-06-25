c3a1ed6f6c63132b77fd73be5d4383a6eb778c02
Austin Jess <ajess33@gmail.com>
2019-06-25 15:44:58 -0500

    initial commit

---
+++ src/components/counter/counter.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Counter from './counter';
+import renderer from 'react-test-renderer';
+import { mount, shallow, render } from 'enzyme';
+
+describe('The Counter Component', () => {
+  it('IS ALIVE!!!', () => {
+    let counter = mount(<Counter />);
+    expect(counter.find('.count').text()).toBe('0');
+  });
+  it('matches snapshot without props', () => {
+    const tree = renderer.create(<Counter />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('can count up', () => {});
+
+  it('can count down', () => {});
+});
